aeb56cc57b281f84fe6c94fb96bb8f6fe8711870
LenzPoetK <lenz.wm55@gmail.com>
2024-09-15 20:57:22 -0300

    adicao de menu funcional

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" ______            _        _______  _______  _______  _          _______  _        _______           _______ ");
+        System.out.println("(  __  \\ |\\     /|( (    /|(  ____ \\(  ____ \\(  ___  )( (    /|  (  ____ \\( \\      (  ___  )|\\     /|(  ____ \\");
+        System.out.println("| (  \\  )| )   ( ||  \\  ( || (    \\/| (    \\/| (   ) ||  \\  ( |  | (    \\/| (      | (   ) || )   ( || (    \\/");
+        System.out.println("| |   ) || |   | ||   \\ | || |      | (__    | |   | ||   \\ | |  | |      | |      | (___) || | _ | || (_____ ");
+        System.out.println("| |   | || |   | || (\\ \\) || | ____ |  __)   | |   | || (\\ \\) |  | |      | |      |  ___  || |( )| |(_____  )");
+        System.out.println("| |   ) || |   | || | \\   || | \\_  )| (      | |   | || | \\   |  | |      | |      | (   ) || || || |      ) |");
+        System.out.println("| (__/  )| (___) || )  \\  || (___) || (____/\\| (___) || )  \\  |  | (____/\\| (____/\\| )   ( || () () |/\\____) |");
+        System.out.println("(______/ (_______)|/    )_)(_______)(_______/(_______)|/    )_)  (_______/(_______/|/     \\|(_______)\\_______)\n");
+
+        System.out.print(">> Press any key to continue <<");
+        scanner.nextLine();
+
+        while(true){
+            char option;
+            String name;
+
+            ClearConsole.clear();
+
+            System.out.println("> Play (P)");
+            System.out.println("> Tutorial (T)");
+            System.out.println("> Exit (E)\n");
+
+            System.out.print("Chose one option: ");
+            option = scanner.next().charAt(0);
+            scanner.nextLine();
+
+            if(option == 'P'){
+                ClearConsole.clear();
+                System.out.println("game");
+                scanner.nextLine();
+            }
+            else if(option == 'T'){
+                ClearConsole.clear();
+                System.out.println("Tutorial\n");
+
+                System.out.print(">> Press any key to return <<");
+                scanner.nextLine();
+            }
+            else if(option == 'E'){
+                ClearConsole.clear();
+                System.out.println("Sair");
+                break;
+            }
+            else{
+                ClearConsole.clear();
+                System.out.println("Select a valid option.");
+                scanner.nextLine();
+            }
+        }
     }
 }
